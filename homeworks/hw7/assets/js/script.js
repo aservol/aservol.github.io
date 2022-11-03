915c43432b5d66345315b4e1eb03200daa560c19
aservoles <armelle.servoles@berkeley.edu>
2022-11-03 20:41:44 +0100

    finished hw7

---
+++ homeworks/hw7/assets/js/script.js
@@ -0,0 +1,27 @@
+function averageThreeNumbers(x, y, z) {
+    let sum = (x + y + z);
+    let x = getRandomNum (50);
+    let y = getRandomNum (50);
+    let z = getRandomNum (50);
+    return avg = sum / 3;
+}
+
+function ThreeVariables (x, y, z) {
+    let x = getRandomNum (20);
+    let y = getRandomNum (10);
+    let z = getRandomNum (13);
+}
+
+
+function createSentence (num, noun) {
+    let num = 12;
+    let noun = "squirrel";
+    let result = "On average, a Berkeley student has " + num + noun ;
+    return result;
+}
+
+function getRandomNum (max) {
+return Math.floor(Math.random() * max);
+}
+console.log(Math.random());
+
