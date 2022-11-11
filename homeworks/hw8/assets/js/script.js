9002e0d467f21ad3e5c151312e070ee915b0256c
aservoles <armelle.servoles@berkeley.edu>
2022-11-11 03:11:21 +0100

    finished hw8

---
+++ homeworks/hw8/assets/js/script.js
@@ -0,0 +1,24 @@
+document.getElementById ("songs-second-half");
+​
+document.getElementById ("songs-second-half");
+let songsSecondHalf = document.getElementById ("songs-second-half");
+
+songsSecondHalf
+
+songsSecondHalf.classList
+
+songsSecondHalf.innerHTML
+
+songsSecondHalf.classList.add("hidden");
+
+songsSecondHalf.classList
+
+songsSecondHalf.classList.remove("hidden");
+
+songsSecondHalf.classList
+
+songsSecondHalf.classList.toggle("hidden")
+
+
+let toggleSongs = songsSecondHalf.classList.toggle("hidden");
+document.getElementById("toggle-button").onclick = toggleSongs;
